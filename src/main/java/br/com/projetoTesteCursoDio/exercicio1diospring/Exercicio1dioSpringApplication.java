a385587172179f69816de42e2eb7b5278151348b
Guigaspaco <yoshiro2@gmail.com>
2022-06-12 14:28:22 -0300

    Second Commit Exercicio DIO

---
+++ src/main/java/br/com/projetoTesteCursoDio/exercicio1diospring/Exercicio1dioSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoTesteCursoDio.exercicio1diospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio1dioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio1dioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetoTesteCursoDio/exercicio1diospring/Exercicio1dioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoTesteCursoDio.exercicio1diospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio1dioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
